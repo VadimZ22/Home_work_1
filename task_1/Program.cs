7082aed4422b3d7fa52cce9b08f9e7fe3d940818
Vadim_Zubenko <VZworkmail@yandex.ru>
2022-09-17 13:05:27 +0700

    task_1

---
+++ task_1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите два числа!");
+int num_1 = int.Parse(Console.ReadLine());
+int num_2 = int.Parse(Console.ReadLine());
+
+if (num_1 < num_2)
+    Console.WriteLine($"max = {num_2}, min = {num_1}");
+if (num_1 > num_2)
+    Console.WriteLine($"max = {num_1}, min = {num_2}");
+else Console.WriteLine("the numbers are equal");
