6b78e5c29b16628f46d63f993f1b36dda7ddd074
Vadim_Zubenko <VZworkmail@yandex.ru>
2022-09-17 23:27:24 +0700

    add task_4

---
+++ task_4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число!");
+double num = double.Parse(Console.ReadLine());
+
+for (int i = 1; i <= num; i++)
+{
+    if (i % 2 == 0) Console.Write($"{i} ");
+    else continue;
+}
